faa852fe9150a3e078f5add77e992206ef217c1a
Ruslan Sayfullin <uraltus@yandex.ru>
2019-01-13 15:47:04 +0500

    B-20 Remove List of Friends Lists

--- src/pages/settings/PrivacySetting.vue
+++ src/pages/settings/PrivacySetting.vue
@@ -7,7 +7,6 @@
             <select @click="onSelectOption" v-model="key">
                 <option value="all-friends">Только друзья</option>
                 <option value="some-friends">Некоторые друзья</option>
-                <option value="some-friends-list">Некоторые списки друзей</option>
             </select>
             <div id="mb-privacy_settings-accepted_list">
                 <span v-for="(item, index) in list_description">
@@ -22,79 +21,64 @@
                 :cancelDialog="cancelModal"
                 :onSave="saveFriendsWhitelist"
         />
-        <FriendsListsWhitelist
-                v-if="modalStatus==MODAL_FRIENDS_LISTS"
-                :cancelDialog="cancelModal"
-                :onSave="saveFriendsListsWhitelist"
-        />
     </div>
 </template>
 
 <script>
 
-import FriendsListsWhitelist from './FriendsListsWhitelist.vue'
 import FriendsWhitelist from './FriendsWhitelist.vue'
 
 const MODAL_NONE = 0
 const MODAL_FRIENDS = 1
-const MODAL_FRIENDS_LISTS = 2
 
 export default {
-  name: 'App',
-  components: {
-    FriendsListsWhitelist,
-    FriendsWhitelist
-  },
-  data:  function(){
-    return {
-        key: "some-friends",
-        list_description: [
-            {
-                title: "Семья"
-            },
-            {
-                title: "Каюмовы"
+    name: 'App',
+    components: {
+        FriendsWhitelist
+    },
+    data:  function(){
+        return {
+            key: "some-friends",
+            list_description: [
+                {
+                    title: "Семья"
+                },
+                {
+                    title: "Каюмовы"
+                }
+            ],
+            hard_key: "some-friends",
+            modalStatus: MODAL_NONE,
+            MODAL_NONE: MODAL_NONE,
+            MODAL_FRIENDS: MODAL_FRIENDS
+        }
+    },
+    methods: {
+        saveFriendsWhitelist: function(){
+            console.log("friends saved");
+            this.modalStatus = MODAL_NONE;
+            this.hard_key = this.key;
+        },
+        saveFriendsListsWhitelist: function(){
+            console.log("friends list saved");
+            this.modalStatus = MODAL_NONE;
+            this.hard_key = this.key;
+        },
+        cancelModal: function(){
+            this.modalStatus = MODAL_NONE;
+            this.key = this.hard_key;
+        },
+        onSelectOption(){
+            switch(this.key){
+                case "all-friends":
+                    this.hard_key = this.key;
+                    break;
+                case "some-friends":
+                    this.modalStatus = MODAL_FRIENDS;
+                    break;
             }
-        ],
-        hard_key: "some-friends",
-        modalStatus: MODAL_NONE,
-        MODAL_NONE: MODAL_NONE,
-        MODAL_FRIENDS: MODAL_FRIENDS,
-        MODAL_FRIENDS_LISTS: MODAL_FRIENDS_LISTS
-    }
-  },
-  methods: {
-    saveFriendsWhitelist: function(){
-        console.log("friends saved");
-        this.modalStatus = MODAL_NONE;
-        this.hard_key = this.key;
-    },
-    saveFriendsListsWhitelist: function(){
-        console.log("friends list saved");
-        this.modalStatus = MODAL_NONE;
-        this.hard_key = this.key;
-    },
-    cancelModal: function(){
-        this.modalStatus = MODAL_NONE;
-        this.key = this.hard_key;
-    },
-    onSelectOption(){
-        switch(this.key){
-            case "all-friends":
-                this.hard_key = this.key;
-                console.log("all-friends");
-                break;
-            case "some-friends":
-                console.log("some-friends");
-                this.modalStatus = MODAL_FRIENDS;
-                break;
-            case "some-friends-list":
-                console.log("some-friends");
-                this.modalStatus = MODAL_FRIENDS_LISTS;
-                break;
         }
     }
-  }
 }
 </script>
 
