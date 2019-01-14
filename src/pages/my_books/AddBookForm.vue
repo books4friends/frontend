8b2f25fc52b83ac3a8855327bda8d984598d4d7b
Ruslan Sayfullin <uraltus@yandex.ru>
2019-01-14 10:25:41 +0500

    B-9 Add New Book Form logic without server

--- src/pages/my_books/AddBookForm.vue
+++ src/pages/my_books/AddBookForm.vue
@@ -1,46 +1,118 @@
 <template>
     <div id="mb-add_book">
-        <form id="mb-add_book-form">
             <h2>Добавить книгу</h2>
             <div id="mb-add_book-form-inputs">
                 <label for="add_title">Название*</label>
                 <div id="add_title_div">
-                    <input name="title" id="add_title" type="text" placeholder="Название">
+                    <input v-model="title"
+                           @keyup="searchGoogle"
+                           @focus="showGoogleSuggestions"
+                           @blur="hideGoogleSuggestions"
+                           name="title" id="add_title" type="text" placeholder="Название">
 
-                    <div id="google_books_suggestion">
-                        <div class="google_book">
-                            <span class="google_book-author">Жан Пюри</span>
-                            <span class="google_book-title">Маленький сын</span>
-                        </div>
-                        <div class="google_book">
-                            <span class="google_book-author">Лана Перишная</span>
-                            <span class="google_book-title">Маленький сын</span>
-                        </div>
-                        <div class="google_book">
-                            <span class="google_book-author">Жан Пюри</span>
-                            <span class="google_book-title">Маленький сын</span>
-                        </div>
-                        <div class="google_book">
-                            <span class="google_book-author">Жан Пюри</span>
-                            <span class="google_book-title">Маленький сын</span>
-                        </div>
-                        <div class="google_book">
-                            <span class="google_book-author">Жан Пюри</span>
-                            <span class="google_book-title">Маленький сын</span>
+                    <div v-if="googleSuggestionsVisible" id="google_books_suggestion">
+                        <div v-for="book in googleBooks"
+                             @click="selectGoogleBook(book)"
+                             class="google_book"
+                        >
+                            <span v-if="book.author" class="google_book-author">{{ book.author }}</span>
+                            <span v-if="book.author">.&nbsp;</span>
+                            <span class="google_book-title">{{ book.title }}</span>
                         </div>
                     </div>
                 </div>
                 <label for="add_author">Автор*</label>
-                <input name="author" id="add_author" type="text" placeholder="Автор">
+                <input v-model="author" name="author" id="add_author" type="text" placeholder="Автор">
                 <label for="add_comment">Комментарий</label>
-                <input name="comment" id="add_comment" type="text" placeholder="Комментарий">
-                <input name="google_id" id="add_google_id" type="hidden">
+                <input v-model="comment" name="comment" id="add_comment" type="text" placeholder="Комментарий">
             </div>
-            <div id="mb-add_book-form-submit"><input type="submit" class="button" value="Добавить"></div>
-        </form>
+            <div id="mb-add_book-form-submit">
+                <AppButton :onClick="submit">Добавить</AppButton>
+            </div>
     </div>
-
 </template>
+
+<script>
+    import AppButton from "../../components/ui/AppButton"
+
+    export default {
+        components: {
+            AppButton
+        },
+        data: function () {
+            return {
+                title: "",
+                author: "",
+                comment: null,
+                selectedGoogleBook: null,
+                googleSuggestionsVisible: false,
+                googleSearchTimeout: null,
+                googleBooks: []
+            }
+        },
+        methods: {
+            searchGoogle: function () {
+                if (this.title === "") {
+                    this.googleBooks = [];
+                    return;
+                }
+                if(this.googleSearchTimeout != null)
+                    clearTimeout(this.googleSearchTimeout);
+                this.googleSearchTimeout = setTimeout(function () {
+                    fetch("https://www.googleapis.com/books/v1/volumes?q=" + this.title + "&maxResults=5")
+                        .then((response) => {
+                            if(response.ok) {
+                                return response.json();
+                            }
+                         })
+                        .then((data) => {
+                            this.googleBooks = [];
+                            if (data.items)
+                                for(let i=0; i<data.items.length; i++){
+                                    let book = {};
+                                    book.id = data.items[i].id;
+                                    if (data.items[i].volumeInfo.hasOwnProperty("authors")){
+                                        book.author = data.items[i].volumeInfo.authors[0];
+                                        book.title = data.items[i].volumeInfo.title;
+                                    }else{
+                                        book.title = data.items[i].volumeInfo.title;
+                                    }
+                                    this.googleBooks.push(book);
+                                }
+                        });
+               }.bind(this), 350);
+            },
+            selectGoogleBook: function (book) {
+                this.title = book.title;
+                this.author = book.author;
+                this.selectedGoogleBook = book;
+            },
+            showGoogleSuggestions: function(){
+                this.googleSuggestionsVisible = true;
+            },
+            hideGoogleSuggestions: function(){
+                setTimeout(function () {
+                    this.googleSuggestionsVisible = false;
+                }.bind(this), 200);
+            },
+            submit: function () {
+                if(!this.title || !this.author)
+                    return;
+
+                if (
+                    this.selectedGoogleBook
+                    && this.title === this.selectedGoogleBook.title
+                    && this.author === this.selectedGoogleBook.author
+                ){
+                    console.log("Add book: ", this.selectedGoogleBook.id, this.title, this.author);
+                }else{
+                    console.log("Add book: ", this.title, this.author);
+                }
+            }
+        }
+    }
+</script>
+
 
 <style scoped>
 #mb-add_book-form-inputs{
