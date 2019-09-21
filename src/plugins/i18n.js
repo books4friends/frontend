fb6f3d8dd2b64922708729c99ade3e804011e261
Ruslan Sayfullin <uraltus@yandex.ru>
2019-09-21 12:01:23 +0500

    GBC-35 Add Determine locale

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -6,8 +6,21 @@ import ru from '../locales/ru'
 
 Vue.use(VueI18n);
 
+const configElement = document.getElementById( 'config' );
+const config = JSON.parse( configElement.innerHTML );
+
+let locale;
+if(config.locale !== 'undefined'){
+    locale = config.locale;
+}else{
+    if (navigator.language === 'ru' || navigator.language === 'RU_ru')
+        locale = 'ru';
+    else
+        locale = 'en';
+}
+
 const i18n = new VueI18n({
-    locale: 'ru',
+    locale: locale,
     messages:{
         en,
         ru
