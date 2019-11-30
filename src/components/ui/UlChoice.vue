6e312b6a8e405eb9b158cc7cf9c9a46cd0e04913
Ruslan Saifullin <uraltus@yandex.ru>
2019-11-30 14:34:37 +0500

    GB-4 Add genre to filter in Friends books list

---
+++ src/components/ui/UlChoice.vue
@@ -0,0 +1,37 @@
+<template>
+    <ul class="ul">
+        <slot></slot>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: "ulChoice.vue"
+    }
+</script>
+
+<style scoped>
+    .ul > li{
+        font-family: -apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif;
+        font-weight: 400;
+        font-size: 13px;
+        color: #656565;
+
+        display: block;
+        min-height: 32px;
+        line-height: 31px;
+        cursor: pointer;
+        padding: 0 5px 0 28px;
+    }
+
+    .ul > li:hover{
+        background-color: #edeef0;
+    }
+
+    .ul > li.selected{
+        border-right: 2px solid #5181b8;
+        font-weight: 800;
+        color: #000;
+        background-color: #edeef0;
+    }
+</style>
---
+++ src/pages/friends_books/FilterByGenre.vue
@@ -0,0 +1,53 @@
+<template>
+    <UlChoice>
+        <li
+                @click="filter(-1)"
+                v-bind:class="{selected: currentIndex===-1}"
+        >
+            {{ $t('filters.all_genres') }}
+        </li>
+        <li
+                @click="filter(genre.index)"
+                v-for="genre in config.genres"
+                v-bind:class="{selected: currentIndex===genre.index}"
+        >
+            {{ genre.title }}
+        </li>
+    </UlChoice>
+</template>
+
+<script>
+    import UlChoice from "../../components/ui/UlChoice";
+    import config from "../../plugins/getDjangoConfig";
+
+    export default {
+        name: "FilterByGenre",
+        components: {
+            UlChoice,
+        },
+        props: {
+            setFilter: {
+                type: Function,
+                required: true
+            },
+            currentIndex: {
+                type: Number,
+                required: true
+            }
+        },
+        data: function(){
+            return {
+                config
+            }
+        },
+        methods: {
+            filter: function (index) {
+                this.setFilter(index);
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
